b749c792335e4784f58778cd08c51404b0506d1c
sainadharyaan786 <sainadharyaan786@gmail.com>
2025-04-16 11:00:58 +0000

    created 1604:4:30pm

--- dotnetapp/Models/UserRoles.cs
+++ dotnetapp/Models/UserRoles.cs
@@ -7,15 +7,10 @@ namespace dotnetapp.Models
 {
     public class UserRoles
     {
-<<<<<<< HEAD
-        public string Guide{get; set;}
-        
-        public string Traveller{get; set;}
-=======
+
         public const string Guide="Guide";
         
         public const string Traveller="Traveller";
->>>>>>> 31f8197351606fe649bdc28bd8f18be3a0054725
         
     }
 }
