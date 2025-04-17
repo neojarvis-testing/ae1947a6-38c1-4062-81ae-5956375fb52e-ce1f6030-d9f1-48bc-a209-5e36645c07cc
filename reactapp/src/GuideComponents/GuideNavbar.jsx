f8888d6a49bdfc522f011a2be0ddd5f8d5408b37
aakash954 <aakashsaini6866@gmail.com>
2025-04-17 13:23:44 +0000

    add

--- reactapp/src/GuideComponents/GuideNavbar.jsx
+++ reactapp/src/GuideComponents/GuideNavbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { Link,useNavigate} from 'react-router-dom';
+import { Link} from 'react-router-dom';
 import './GuideNavbar.css';
-import { useNavigate } from 'react-router-dom';
+import { navigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LogoutModel from './LogoutModel';
 
