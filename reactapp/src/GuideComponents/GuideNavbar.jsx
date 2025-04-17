438dd994c6daaaa37d7b62359f1f22fee2631a2b
NilagiriPavithra <nilagiripavithra@gmail.com>
2025-04-17 13:16:55 +0000

    view placeee

--- reactapp/src/GuideComponents/GuideNavbar.jsx
+++ reactapp/src/GuideComponents/GuideNavbar.jsx
@@ -32,12 +32,6 @@ const GuideNavbar = () => {
                         <ul className="navbar-nav ms-auto">
 
                             <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-  
-
-                            <select>
-                            
-
-
                             <li className="nav-item"><Link className="nav-link" to="/profile">Demoguide/Guide</Link></li>
                                 <li className="nav-item">
                                     <button className="btn btn-primary btn-block" onClick={handleLogoutClick}>Logout</button>
