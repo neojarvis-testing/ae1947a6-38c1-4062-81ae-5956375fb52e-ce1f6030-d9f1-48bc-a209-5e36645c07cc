7ce7b943dd3ea3657acab82b9d1fb85b1868523b
NilagiriPavithra <nilagiripavithra@gmail.com>
2025-04-18 03:45:49 +0000

    Hello Team

--- reactapp/src/GuideComponents/GuideNavbar.jsx
+++ reactapp/src/GuideComponents/GuideNavbar.jsx
@@ -26,7 +26,6 @@ const GuideNavbar = () => {
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    </div>
 
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav ms-auto">
