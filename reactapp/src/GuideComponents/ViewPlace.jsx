134638f5edbe8a4daee3acfd445d1393433342d8
NilagiriPavithra <nilagiripavithra@gmail.com>
2025-04-18 04:29:33 +0000

    Hello Team

--- reactapp/src/GuideComponents/ViewPlace.jsx
+++ reactapp/src/GuideComponents/ViewPlace.jsx
@@ -66,55 +66,6 @@ const ViewPlace = () => {
     const handleEdit = (id) => {
         navigate(`/edit/${id}`);
     };
-
-  return (
-    <div>
-      <GuideNavbar/>
-        <h2 style={{textAlign:"center"}}>Places</h2>
-        {successMessage && <p class="text-success"><h2>{successMessage}</h2></p>}
-        {errors && <p class="text-danger"><h2>{errors}</h2></p>}
-        {loading && <p>Loading...</p>}
-        {!loading && !errors && <p><h2>{errors}</h2></p>}
-        <table class="table table-light table-striped" >
-                      <thead>
-                        <tr>
-                          <th >Image</th>
-                          <th >Name</th>
-                          <th >Category</th>
-                          <th>Location</th>
-                          <th>Best time to visit</th>
-                          <th>Action</th>
-                        </tr>
-                      </thead>
-          {place.length===0 ? (
-            <p>No places available</p>
-          ) : (
-            <ul>
-              {Array.isArray(place) && place.map((myPlace)=>(
-                <li key={myPlace.PlaceId || myPlace.Name}>
-                  <div>
-                    
-                      <tbody>
-                        <tr>
-                          <td ><p>{myPlace.PlaceImage}</p></td>
-                          <td ><p>{myPlace.Name}</p></td>
-                          <td ><p>{myPlace.Category}</p></td>
-                          <td ><p>{myPlace.Location}</p></td>
-                          <td > <p>{myPlace.BestTimeToVisit}</p></td>
-                          <td>
-                            <button onClick={()=>handleEdit(myPlace.PlaceId)}  class="btn btn-primary ">Edit</button>
-                            <button onClick={()=> handleDelete(myPlace.PlaceId)} class="btn btn-danger">Delete</button>
-                          </td>
-                        </tr>
-                      </tbody>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          )}
-          </table>
-          
-    
     return (
         <div>
             <GuideNavbar />
